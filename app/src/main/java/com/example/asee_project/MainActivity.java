5d08932e17587506bd103979ada9715064bdaf04
AbelCano <acanobar@alumnos.unex.es>
2020-11-04 13:17:22 +0100

    commit del abel

--- app/src/main/java/com/example/asee_project/MainActivity.java
+++ app/src/main/java/com/example/asee_project/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.asee_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-//commit el abel
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Commit-prueba","Commit el abel");
     }
 }
